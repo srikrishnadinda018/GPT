cc5a4ef0d9873d379914124416e69e3342c6037a
srikrishnadinda018 <127649098+srikrishnadinda018@users.noreply.github.com>
2023-11-20 07:41:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
-function sendMessage() {
+// script.js
+async function sendMessage() {
   const userInput = document.getElementById('user-input').value;
   const chatOutput = document.getElementById('chat-output');
+
+  if (!userInput) {
+    return; // Do not send empty messages
+  }
 
   // Display user's message
   chatOutput.innerHTML += `<div>User: ${userInput}</div>`;
 
-  // Call Wikipedia API
-  getWikipediaData(userInput)
-    .then(response => {
-      // Display Wikipedia results
-      chatOutput.innerHTML += `<div>Wikipedia: ${response}</div>`;
-    })
-    .catch(error => {
-      console.error(error);
-      chatOutput.innerHTML += `<div>Error fetching Wikipedia data.</div>`;
-    });
+  try {
+    // Call Wikipedia API
+    const response = await getWikipediaData(userInput);
+
+    // Display Wikipedia results
+    chatOutput.innerHTML += `<div>Wikipedia: ${response}</div>`;
+  } catch (error) {
+    console.error(error);
+    chatOutput.innerHTML += `<div>Error fetching Wikipedia data.</div>`;
+  }
 
   // Clear input field
   document.getElementById('user-input').value = '';
@@ -26,9 +31,16 @@ async function getWikipediaData(query) {
   try {
     const response = await fetch(apiUrl);
     const data = await response.json();
+
     const pageId = Object.keys(data.query.pages)[0];
+
+    if (pageId === '-1') {
+      throw new Error('No information found for the given query.');
+    }
+
     return data.query.pages[pageId].extract;
   } catch (error) {
     throw new Error('Failed to fetch Wikipedia data.');
   }
 }
+
