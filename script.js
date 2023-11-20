a0108ab0a404780f64a066284b64fe65ddaf0ce8
srikrishnadinda018 <127649098+srikrishnadinda018@users.noreply.github.com>
2023-11-20 07:49:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,22 +11,42 @@ async function sendMessage() {
   chatOutput.innerHTML += `<div>User: ${userInput}</div>`;
 
   try {
-    // Call Wikipedia API
-    const response = await getWikipediaData(userInput);
+    // Call Wikipedia API to search for the query
+    const searchResults = await searchWikipedia(userInput);
+
+    if (searchResults.length === 0) {
+      throw new Error('No results found for the given query.');
+    }
+
+    // Get details for the most relevant result
+    const mostRelevantResult = searchResults[0];
+    const response = await getWikipediaData(mostRelevantResult.title);
 
     // Display Wikipedia results
     chatOutput.innerHTML += `<div>Wikipedia: ${response}</div>`;
   } catch (error) {
     console.error(error);
-    chatOutput.innerHTML += `<div>Error fetching Wikipedia data.</div>`;
+    chatOutput.innerHTML += `<div>Error: ${error.message}</div>`;
   }
 
   // Clear input field
   document.getElementById('user-input').value = '';
 }
 
-async function getWikipediaData(query) {
-  const apiUrl = `https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&exintro&titles=${query}&origin=*`;
+async function searchWikipedia(query) {
+  const apiUrl = `https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=${query}&origin=*`;
+
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    return data.query.search;
+  } catch (error) {
+    throw new Error('Failed to perform the search.');
+  }
+}
+
+async function getWikipediaData(title) {
+  const apiUrl = `https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&exintro&titles=${title}&origin=*`;
 
   try {
     const response = await fetch(apiUrl);
@@ -35,7 +55,7 @@ async function getWikipediaData(query) {
     const pageId = Object.keys(data.query.pages)[0];
 
     if (pageId === '-1') {
-      throw new Error('No information found for the given query.');
+      throw new Error('No information found for the given title.');
     }
 
     return data.query.pages[pageId].extract;
@@ -43,4 +63,3 @@ async function getWikipediaData(query) {
     throw new Error('Failed to fetch Wikipedia data.');
   }
 }
-
